90bf85fd8d7ac7ad70615d8bb486ec5dfdc55609
sojung <sojung528@gmail.com>
2023-04-19 11:22:04 +0900

    Init: 대표 색상 세팅

---
+++ lib/core/constants/theme.dart
@@ -0,0 +1,8 @@
+//Color 설정
+import 'package:flutter/material.dart';
+
+const donutColor1 = Color(0xFFFBE1C6);
+const donutColor2 = Color(0xFFEFC392);
+const donutColorBasic = Color(0xFFD98032);
+const donutColorPoint = Color(0xFFBF4904);
+const donutColorBase = Color(0xFFA65A2E);
