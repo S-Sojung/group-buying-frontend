c2a1a5fbf26551acd3435fa39e489bab624c8825
sojung <sojung528@gmail.com>
2023-04-19 12:21:22 +0900

    Style: donut Color 색상 추가

--- lib/core/constants/theme.dart
+++ lib/core/constants/theme.dart
@@ -1,6 +1,8 @@
 //Color 설정
 import 'package:flutter/material.dart';
 
+const donutGray100 = Color(0xFFD9D9D9);
+const donutGray200 = Color(0xFF999999);
 const donutColor1 = Color(0xFFFBE1C6);
 const donutColor2 = Color(0xFFEFC392);
 const donutColorBasic = Color(0xFFD98032);
