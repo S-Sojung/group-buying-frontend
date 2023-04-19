401c4fd8894e2770537e04b980551418d074ea32
sojung <sojung528@gmail.com>
2023-04-19 10:56:10 +0900

    Init: text style 및 font 설정

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'HallymGothic',
+      ),
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
@@ -21,7 +24,9 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Text("안녕하세요 한림고딕체입니다.")
+    );
   }
 }
-
