c036e9a5b752f9aeb2b2bbe52d3d4258046793f3
sojung <sojung528@gmail.com>
2023-04-20 11:22:19 +0900

    Feat: slim 버전 formfield 생성, donut_result_text_field 생성

---
+++ lib/views/components/donut_result_text_field.dart
@@ -0,0 +1,41 @@
+import 'package:donut/core/constants/size.dart';
+import 'package:donut/core/constants/style.dart';
+import 'package:donut/core/constants/theme.dart';
+import 'package:flutter/material.dart';
+
+class DonutResultTextField extends StatelessWidget {
+  final String title;
+  final int price;
+
+  const DonutResultTextField({
+    required this.title,
+    required this.price,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+          horizontal: getScreenWidth(context) * 0.05, vertical: 5),
+      child: Container(
+        height: 40,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(color: donutColorBase),
+            color: donutColor1
+        ),
+        child: Row(children: [
+          Text(
+            " ${title}",
+            style: callout(),
+          ),
+          Spacer(),
+          Text(
+            " ${price} 원 ",
+            style: callout(),
+          ),
+        ]),
+      ),
+    );
+  }
+}
--- lib/views/components/donut_text_form_field.dart
+++ lib/views/components/donut_text_form_field.dart
@@ -54,3 +54,57 @@ class DonutTextFormField extends StatelessWidget {
     );
   }
 }
+
+
+class DonutTextFormFieldSlim extends StatelessWidget {
+  final String title;
+  final String hint;
+  final funValidator;
+  final controller;
+
+  const DonutTextFormFieldSlim({
+    required this.title,
+    this.hint = "",
+    required this.funValidator,
+    this.controller,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: getScreenWidth(context)*0.05,vertical: 5),
+      child: Column(
+        crossAxisAlignment:  CrossAxisAlignment.start,
+        children: [
+          Text("${title}",style: callout(),),
+          Container(
+            height: 40,
+            child: TextFormField(
+              textAlign: TextAlign.start,
+              controller: controller,
+              validator: funValidator,
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: donutColorBasic),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: donutColorBasic),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                errorBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: donutColorBasic),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                focusedErrorBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: donutColorBasic),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
