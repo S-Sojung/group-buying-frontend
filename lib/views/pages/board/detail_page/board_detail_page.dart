3ebf9c0c4938634b02ab9e6ec9c4930a7358bc0a
sojung <sojung528@gmail.com>
2023-04-25 15:55:19 +0900

    Feat: 앱바 상단 버튼 수정하기 및 신고하기 추가

--- lib/views/pages/board/detail_page/board_detail_page.dart
+++ lib/views/pages/board/detail_page/board_detail_page.dart
@@ -1,14 +1,7 @@
-import 'package:donut/core/constants/size.dart';
-import 'package:donut/core/constants/style.dart';
 import 'package:donut/core/constants/theme.dart';
 import 'package:donut/model/board/board.dart';
-import 'package:donut/model/event/event.dart';
-import 'package:donut/model/rate/rate.dart';
-import 'package:donut/views/components/donut_rate_bar.dart';
 import 'package:donut/views/pages/board/detail_page/component/board_detail_body.dart';
-import 'package:donut/views/pages/board/home_page/component/board_category_appbar.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class BoardDetailPage extends StatelessWidget {
   final Board board;
@@ -18,17 +11,71 @@ class BoardDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white10,
-          elevation: 0,
-          foregroundColor: donutColorBasic,
-          actions: [IconButton(onPressed: () {
-
-          }, icon: Icon(Icons.more_vert))],
-        ),
-        body: BoardDetailBody(
-          board: board,
-        ),
+      appBar: AppBar(
+        backgroundColor: Colors.white10,
+        elevation: 0,
+        foregroundColor: donutColorBasic,
+        actions: [DonutPopupMenuButton(),DonutPopupOwnMenuButton()],
+      ),
+      body: BoardDetailBody(
+        board: board,
+      ),
     );
   }
 }
+
+
+enum MenuTypeOwn {
+  first
+}
+
+class DonutPopupMenuButton extends StatelessWidget {
+
+  const DonutPopupMenuButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton<MenuTypeOwn>(
+      onSelected: (MenuTypeOwn result) {
+        // 신고하는 곳으로
+        // Navigator.push(context, MaterialPageRoute(builder: (context) => ,));
+        print("신고하기");
+      },
+      itemBuilder: (BuildContext buildContext) {
+        return [
+          for (final value in MenuTypeOwn.values)
+            PopupMenuItem(
+              value: value,
+              child: Text("신고하기"),
+            )
+        ];
+      },
+    );
+  }
+}
+
+class DonutPopupOwnMenuButton extends StatelessWidget {
+
+  const DonutPopupOwnMenuButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton<MenuTypeOwn>(
+      onSelected: (MenuTypeOwn result) {
+        // 수정페이지
+        // Navigator.push(context, MaterialPageRoute(builder: (context) => ,));
+        print("수정하기");
+      },
+      itemBuilder: (BuildContext buildContext) {
+        return [
+          for (final value in MenuTypeOwn.values)
+            PopupMenuItem(
+              value: value,
+              child: Text("수정하기"),
+            )
+        ];
+      },
+    );
+  }
+}
+
