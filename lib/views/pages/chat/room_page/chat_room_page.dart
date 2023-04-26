d5a9a9a2412edda997f8c3c19378239263b8397a
sojung <sojung528@gmail.com>
2023-04-26 19:33:35 +0900

    Feat: 채팅룸 헤더 고정 및 경고문구 리스트뷰 안에 고정

--- lib/views/pages/chat/room_page/chat_room_page.dart
+++ lib/views/pages/chat/room_page/chat_room_page.dart
@@ -1,6 +1,7 @@
 import 'package:donut/core/constants/theme.dart';
 import 'package:donut/model/user/user.dart';
 import 'package:donut/views/components/donut_round_tag.dart';
+import 'package:donut/views/pages/chat/room_page/components/chat_room_header.dart';
 import 'package:donut/views/pages/chat/room_page/components/donut_chat.dart';
 import 'package:flutter/material.dart';
 
@@ -36,15 +37,8 @@ class _ChatRoomPageState extends State<ChatRoomPage> {
       body: Container(
         child: Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: DonutRoundTag("유저1,유저2 님과의 대화입니다."),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(5),
-              child: DonutRoundTag("도넛 마켓은 당사자간의 거래에는 개입하지 않으며 페이 사용 시 거래 확정 후 개설자에게 돈이 지급 됩니다.",code: "1"),
-            ),
-            _buildChatList(),
+            ChatRoomHeader(),
+            _buildChatList(), // 채팅 리스트
             Divider(
               height: 1.0,
             ),
@@ -64,9 +58,23 @@ class _ChatRoomPageState extends State<ChatRoomPage> {
       child: ListView.builder(
         padding: EdgeInsets.all(8.0),
         reverse: true,
-        itemCount: _messages.length,
-        itemBuilder: (BuildContext context, int index) {
+        itemCount: _messages.length +1,
+        itemBuilder: (BuildContext context, int index ) {
           // 유저가 현재 로그인한 내정보인지
+          if(index == _messages.length){
+            return Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: DonutRoundTag("유저1,유저2 님과의 대화입니다."),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: DonutRoundTag("도넛 마켓은 당사자간의 거래에는 개입하지 않으며 페이 사용 시 거래 확정 후 개설자에게 돈이 지급 됩니다.",code: "1"),
+                ),
+              ],
+            );
+          }
           return DonutChat(message:_messages[index], user: users[0]);
         },
       ),
