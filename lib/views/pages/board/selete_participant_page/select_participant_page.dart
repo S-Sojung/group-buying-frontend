d50e9c9df4c1b47b0b37500058e50f7cba7ffa6d
sojung <sojung528@gmail.com>
2023-04-26 11:55:58 +0900

    Style: 오타 수정

--- lib/views/pages/board/selete_participant_page/select_participant_page.dart
+++ lib/views/pages/board/selete_participant_page/select_participant_page.dart
@@ -1,15 +1,10 @@
-import 'package:donut/core/constants/size.dart';
-import 'package:donut/core/constants/style.dart';
 import 'package:donut/core/constants/theme.dart';
-import 'package:donut/model/board/board.dart';
-import 'package:donut/model/event/event.dart';
-import 'package:donut/views/components/donut_round_tag.dart';
 import 'package:donut/views/pages/board/selete_participant_page/component/select_participant_body.dart';
 import 'package:donut/views/pages/board/selete_participant_page/component/select_participant_header.dart';
 import 'package:flutter/material.dart';
 
-class SeleteParticipantPage extends StatelessWidget {
-  const SeleteParticipantPage({Key? key}) : super(key: key);
+class SelectParticipantPage extends StatelessWidget {
+  const SelectParticipantPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +18,6 @@ class SeleteParticipantPage extends StatelessWidget {
           ),
           SeleteParticipantHeader(),
           SelectParticipantBody(),
-
         ],
       ),
     );
