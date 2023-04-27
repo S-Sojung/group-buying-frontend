d52f96f3202f1625ab9e58024a575ca9858c9923
sojung <sojung528@gmail.com>
2023-04-27 09:58:49 +0900

    Feat: 구매내역 페이지 이동 및 탭바 구현

--- lib/views/pages/user/purchase_history_page/user_purchase_history_page.dart
+++ lib/views/pages/user/purchase_history_page/user_purchase_history_page.dart
@@ -1,10 +1,75 @@
+import 'package:donut/core/constants/theme.dart';
+import 'package:donut/model/board/board.dart';
+import 'package:donut/views/components/board_list.dart';
 import 'package:flutter/material.dart';
 
-class UserPurchaseHistoryPage extends StatelessWidget {
+class UserPurchaseHistoryPage extends StatefulWidget {
   const UserPurchaseHistoryPage({Key? key}) : super(key: key);
 
   @override
+  State<UserPurchaseHistoryPage> createState() =>
+      _UserPurchaseHistoryPageState();
+}
+
+class _UserPurchaseHistoryPageState extends State<UserPurchaseHistoryPage>
+    with SingleTickerProviderStateMixin {
+  TabController? _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(length: 2, vsync: this);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.white10,
+          foregroundColor: donutColorBasic,
+          title: Text("구매 내역"),
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.notifications,
+                  size: 30,
+                )),
+          ]),
+      body: Column(
+        children: [
+          TabBar(controller: _tabController, tabs: [
+            Tab(
+              text: "구매 중",
+            ),
+            Tab(
+              text: "구매 완료",
+            ),
+          ]),
+          Expanded(
+            child: TabBarView(
+              controller: _tabController,
+              children: [
+                CustomScrollView(
+                  slivers: [
+                    BoardList(
+                      boards: boards,
+                    ),
+                  ],
+                ),
+                CustomScrollView(
+                  slivers: [
+                    BoardList(
+                      boards: boards,
+                    ),
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
