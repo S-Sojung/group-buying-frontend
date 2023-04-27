c9b5c19048fbeb5b5a812fe4dd94ac1ffdcf902d
sojung <sojung528@gmail.com>
2023-04-27 09:58:04 +0900

    Feat: list tile 공용컴포넌트로 변경

---
+++ lib/views/components/board_list.dart
@@ -0,0 +1,28 @@
+import 'package:donut/core/constants/size.dart';
+import 'package:donut/model/board/board.dart';
+import 'package:donut/views/components/board_list_tile.dart';
+import 'package:flutter/material.dart';
+
+class BoardList extends StatelessWidget {
+  final listSize;
+  final List<Board> boards;
+  const BoardList({
+    this.listSize = 1.0, required this.boards, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: Container(
+        width: double.infinity,
+        height: getScreenHeigth(context)*listSize,
+        child: ListView.builder(
+          itemCount: boards.length, // 선택한 게시글 갯수
+          itemBuilder: (context, index) {
+            //게시글 넘겨주기
+            return BoardListTile(boards[index]);
+          },
+        ),
+      ),
+    );
+  }
+}
