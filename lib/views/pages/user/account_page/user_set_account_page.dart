cd411cb5d4696d8a0abf8b4adc97f76f59c743bf
sojung <sojung528@gmail.com>
2023-04-28 19:15:39 +0900

    Feat: 본인 계좌 설정

--- lib/views/pages/user/account_page/user_set_account_page.dart
+++ lib/views/pages/user/account_page/user_set_account_page.dart
@@ -1,3 +1,5 @@
+import 'package:donut/core/constants/theme.dart';
+import 'package:donut/views/pages/user/account_page/components/user_set_account_body.dart';
 import 'package:flutter/material.dart';
 
 class UserSetAccountPage extends StatelessWidget {
@@ -5,6 +7,21 @@ class UserSetAccountPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.white10,
+          foregroundColor: donutColorBasic,
+          title: Text("계좌 번호"),
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.notifications,
+                  size: 30,
+                )),
+          ]),
+      body: UserSetAccountBody(),
+    );
   }
 }
