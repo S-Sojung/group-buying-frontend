38e05dc16a4f7c5ee6f776e704f7085a1cea8487
sojung <sojung528@gmail.com>
2023-05-01 09:00:51 +0900

    Feat: 관심 게시글 목록 설정

--- lib/views/pages/user/wishlist_page/user_wishlist_page.dart
+++ lib/views/pages/user/wishlist_page/user_wishlist_page.dart
@@ -1,12 +1,37 @@
+import 'package:donut/core/constants/theme.dart';
+import 'package:donut/model/board/board.dart';
+import 'package:donut/views/components/board_list.dart';
 import 'package:flutter/material.dart';
 
 
 class UserWishlistPage extends StatelessWidget {
 
-  const UserWishlistPage({Key? key}) : super(key: key);
+  UserWishlistPage({Key? key}) : super(key: key);
 
+  List<Board> wishboards = boards;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar:AppBar(
+          elevation: 0,
+          backgroundColor: Colors.white10,
+          foregroundColor: donutColorBasic,
+          title: Text("관심 게시글 목록"),
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.notifications,
+                  size: 30,
+                )),
+          ]),
+      body: CustomScrollView(
+        slivers: [
+          BoardList(
+            boards: boards,
+          ),
+        ],
+      ),
+    );
   }
 }
