64c25df7991569cb57b51e459b1bda4887202c31
sojung <sojung528@gmail.com>
2023-05-01 12:39:26 +0900

    Feat: 관심 카테고리 삭제 구현

--- lib/views/pages/user/wishlist_page/user_wishlist_page.dart
+++ lib/views/pages/user/wishlist_page/user_wishlist_page.dart
@@ -1,6 +1,7 @@
 import 'package:donut/core/constants/theme.dart';
 import 'package:donut/model/board/board.dart';
 import 'package:donut/views/components/board_list.dart';
+import 'package:donut/views/components/donut_appbar.dart';
 import 'package:flutter/material.dart';
 
 
@@ -12,19 +13,7 @@ class UserWishlistPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
-          elevation: 0,
-          backgroundColor: Colors.white10,
-          foregroundColor: donutColorBasic,
-          title: Text("관심 게시글 목록"),
-          actions: [
-            IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.notifications,
-                  size: 30,
-                )),
-          ]),
+      appBar:subAppbar("관심 게시글 목록"),
       body: CustomScrollView(
         slivers: [
           BoardList(
