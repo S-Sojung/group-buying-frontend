dd7fbafc4108c101b635c1e1f6734fd55017611f
sojung <sojung528@gmail.com>
2023-05-01 15:58:28 +0900

    Feat: 관심 카테고리 코드 정리

--- lib/views/pages/user/category_page/user_category_page.dart
+++ lib/views/pages/user/category_page/user_category_page.dart
@@ -45,85 +45,11 @@ class _UserCategoryPageState extends State<UserCategoryPage> {
                   maxCrossAxisExtent: 100, mainAxisExtent: 140),
               itemBuilder: (context, index) {
                 return index < myCategorys.length
-                    ? InkWell(
-                        onLongPress: () {
-                          setState(() {
-                            notMyCategorylist.add(categorys[myCategorys[index].categoryId-1]);
-                            myCategorys.removeAt(index);
-                          });
-                        },
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20),
-                                border: Border.all(color: donutColorBasic),
-                                color: Colors.white),
-                            child: Padding(
-                              padding: const EdgeInsets.all(10),
-                              child: Column(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Image.asset(
-                                      "assets/categories/${category_eng[myCategorys[index].categoryId - 1]}.jpg"),
-                                  Text(
-                                    "${categorys[myCategorys[index].categoryId - 1].name}",
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                      )
+                    ? _donutCategoryButton(index)
                     : Padding(
                         padding: const EdgeInsets.all(8.0),
-                        child: myCategorys.length < categorys.length ? DonutButton(
-                            text: "╋",
-                            funPageRoute: () {
-                              return showDialog<String>(
-                                context: context,
-                                builder: (BuildContext context) => AlertDialog(
-                                  title: const Text('추가할 카테고리 선택하기'),
-                                  content: GroupButton(
-                                    options: GroupButtonOptions(
-                                        borderRadius: BorderRadius.circular(10),
-                                        selectedColor: donutColorBasic),
-                                    isRadio: true,
-                                    onSelected: (value, index, isSelected) {
-                                      selectCategory = notMyCategorylist[index];
-                                    },
-                                    buttons:
-                                        notMyCategorylist.map((e) => e.name).toList(),
-                                  ),
-                                  actions: <Widget>[
-                                    TextButton(
-                                      onPressed: ()
-                                  { selectCategory = null;
-                                    return Navigator.pop(context, 'Cancel');},
-                                      child: const Text('Cancel'),
-                                    ),
-                                    TextButton(
-                                      onPressed: () {
-
-                                        setState(() {
-                                          myCategorys.add(MyCatagory(
-                                              id: 6,
-                                              userId: 1,
-                                              categoryId: selectCategory!.id,
-                                              createdAt: DateTime.now()));
-                                          notMyCategorylist
-                                              .remove(selectCategory);
-                                          selectCategory = null;
-                                          Navigator.pop(context, 'OK');
-                                        });
-                                      },
-                                      child: const Text('OK'),
-                                    ),
-                                  ],
-                                ),
-                              );
-                            }) : Container(),
+                        child: myCategorys.length < categorys.length ? _donutAddCategoryButton(context)
+                            : Container(),
                       );
               },
             ),
@@ -131,5 +57,88 @@ class _UserCategoryPageState extends State<UserCategoryPage> {
         ],
       ),
     );
+  }
+
+  InkWell _donutCategoryButton(int index) {
+    return InkWell(
+                      onLongPress: () {
+                        setState(() {
+                          notMyCategorylist.add(categorys[myCategorys[index].categoryId-1]);
+                          myCategorys.removeAt(index);
+                        });
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              border: Border.all(color: donutColorBasic),
+                              color: Colors.white),
+                          child: Padding(
+                            padding: const EdgeInsets.all(10),
+                            child: Column(
+                              mainAxisAlignment:
+                                  MainAxisAlignment.spaceBetween,
+                              children: [
+                                Image.asset(
+                                    "assets/categories/${category_eng[myCategorys[index].categoryId - 1]}.jpg"),
+                                Text(
+                                  "${categorys[myCategorys[index].categoryId - 1].name}",
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+  }
+
+  DonutButton _donutAddCategoryButton(BuildContext context) {
+    return DonutButton(
+                          text: "╋",
+                          funPageRoute: () {
+                            return showDialog<String>(
+                              context: context,
+                              builder: (BuildContext context) => AlertDialog(
+                                title: const Text('추가할 카테고리 선택하기'),
+                                content: GroupButton(
+                                  options: GroupButtonOptions(
+                                      borderRadius: BorderRadius.circular(10),
+                                      selectedColor: donutColorBasic),
+                                  isRadio: true,
+                                  onSelected: (value, index, isSelected) {
+                                    selectCategory = notMyCategorylist[index];
+                                  },
+                                  buttons:
+                                      notMyCategorylist.map((e) => e.name).toList(),
+                                ),
+                                actions: <Widget>[
+                                  TextButton(
+                                    onPressed: ()
+                                { selectCategory = null;
+                                  return Navigator.pop(context, 'Cancel');},
+                                    child: const Text('Cancel'),
+                                  ),
+                                  TextButton(
+                                    onPressed: () {
+
+                                      setState(() {
+                                        myCategorys.add(MyCatagory(
+                                            id: 6,
+                                            userId: 1,
+                                            categoryId: selectCategory!.id,
+                                            createdAt: DateTime.now()));
+                                        notMyCategorylist
+                                            .remove(selectCategory);
+                                        selectCategory = null;
+                                        Navigator.pop(context, 'OK');
+                                      });
+                                    },
+                                    child: const Text('OK'),
+                                  ),
+                                ],
+                              ),
+                            );
+                          });
   }
 }
