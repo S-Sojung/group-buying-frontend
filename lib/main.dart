8f5822ec2941e356d7eb6b0ef51cc38234d21ef4
sojung <sojung528@gmail.com>
2023-05-01 10:35:49 +0900

    Feat: lottie를 사용한 splash 화면 생성

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:donut/core/constants/move.dart';
-import 'package:donut/views/pages/board/home_page/board_home_page.dart';
+import 'package:donut/views/pages/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  //WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
   // SessionUser sessionUser = await UserRepository().fetchJwtVerify();
   // runApp(
   //   ProviderScope(
@@ -24,6 +24,7 @@ class MyApp extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     //SessionUser sessionUser = ref.read(sessionProvider);
     return MaterialApp(
+      title: "donut market",
       theme: ThemeData(
         fontFamily: 'HallymGothic',
       ),
@@ -31,7 +32,7 @@ class MyApp extends ConsumerWidget {
       // 로그인 상태일 경우, 내 위치 가 설정되어 있지 않다면? 내 위치 설정으로 가기
       // initialRoute: sessionUser.isLogin! ? ( 1 == 1 ? Move.boardHomePage : Move.boardHomePage) : Move.loginPage,
       routes: getRouters(),
-      home: BoardHomePage(),
+      home: SplashPage(),
     );
   }
 }
