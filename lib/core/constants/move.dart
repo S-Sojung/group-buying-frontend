f683b093194c776e2c0b781473a454732fc7018c
sojung <sojung528@gmail.com>
2023-05-02 19:35:22 +0900

    Feat: 세션 provider 수정

--- lib/core/constants/move.dart
+++ lib/core/constants/move.dart
@@ -3,6 +3,7 @@ import 'package:donut/views/pages/auth/login_page/login_page.dart';
 import 'package:donut/views/pages/board/home_page/board_home_page.dart';
 import 'package:donut/views/pages/board/map_page/board_map_page.dart';
 import 'package:donut/views/pages/board/write_page/board_write_first_page.dart';
+import 'package:donut/views/pages/splash/after_splash_page/after_splash_page.dart';
 import 'package:flutter/material.dart';
 
 
@@ -12,6 +13,7 @@ class Move {
 
   static String joinPage = "/join";
   static String loginPage = "/login";
+  static String afterSplashPage = "/afterSplashPage";
   static String boardHomePage = "/board/home";
   static String boardMapPage = "/board/map";
   static String boardWritePage = "/board/write";
@@ -25,5 +27,6 @@ Map<String, Widget Function(BuildContext)> getRouters() {
     Move.loginPage: (context) => LoginPage(),
     Move.boardHomePage: (context) => BoardHomePage(),
     Move.boardWritePage: (context) => BoardWriteFirstPage(),
+    Move.afterSplashPage: (context) => AfterSplashPage(),
   };
 }
