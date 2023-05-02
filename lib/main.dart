11be8b0e995e387192e5627a279626137fd2eb61
sojung <sojung528@gmail.com>
2023-05-02 19:36:04 +0900

    Feat: 로그인 폼필드 수정 및 첫 화면에서 로그인 정보 들고오기

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,24 @@
 import 'package:donut/core/constants/move.dart';
-import 'package:donut/views/pages/report/report_page/report_page.dart';
+import 'package:donut/model/user/user_repository.dart';
+import 'package:donut/provider/session_provider.dart';
 import 'package:donut/views/pages/splash/splash_page/splash_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // SessionUser sessionUser = await UserRepository().fetchJwtVerify();
-  // runApp(
-  //   ProviderScope(
-  //     overrides: [
-  //       sessionProvider.overrideWithValue(sessionUser)
-  //     ],
-  //     child: MyApp(),
-  //   ),
-  // );
-  runApp(const MyApp());
+  SessionUser sessionUser = await UserRepository().fetchJwtVerify();
+
+  runApp(
+    ProviderScope(
+      overrides: [
+        sessionProvider.overrideWithValue(sessionUser)
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends ConsumerWidget {
@@ -29,12 +32,12 @@ class MyApp extends ConsumerWidget {
       theme: ThemeData(
         fontFamily: 'HallymGothic',
       ),
+      navigatorKey: navigatorKey,
       debugShowCheckedModeBanner: false,
       // 로그인 상태일 경우, 내 위치 가 설정되어 있지 않다면? 내 위치 설정으로 가기
       // initialRoute: sessionUser.isLogin! ? ( 1 == 1 ? Move.boardHomePage : Move.boardHomePage) : Move.loginPage,
       routes: getRouters(),
       home: SplashPage(),
-      // home: ReportPage(),
     );
   }
 }
