4fbc0fbdaa71d9e371a2f5c27dfe7145aae3e405
sojung <sojung528@gmail.com>
2023-05-08 12:13:46 +0900

    Feat: 내 계좌 세팅

--- lib/dto/my_page/my_account_dto.dart
+++ lib/dto/my_page/my_account_dto.dart
@@ -1,26 +1,20 @@
-class MyAccount {
-  int id;
-  int userId;
+
+
+class MyAccountReqDTO {
   String brand;
   String accountNumber;
 
-  MyAccount({
-    required this.id,
-    required this.userId,
+  MyAccountReqDTO({
     required this.brand,
     required this.accountNumber,
   });
 
-  factory MyAccount.fromJson(Map<String, dynamic> json) => MyAccount(
-    id: json["id"],
-    userId: json["userId"],
+  factory MyAccountReqDTO.fromJson(Map<String, dynamic> json) => MyAccountReqDTO(
     brand: json["brand"],
     accountNumber: json["accountNumber"],
   );
 
   Map<String, dynamic> toJson() => {
-    "id": id,
-    "userId": userId,
     "brand": brand,
     "accountNumber": accountNumber,
   };
